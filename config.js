5faee5c77e46eaa6fda0b36b2694848510050570
sugarkitcare <realimkit@gmail.com>
2026-03-25 15:34:06 +0900

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.json({
-    supabaseUrl: process.env.SUPABASE_URL || '',
-    supabaseKey: process.env.SUPABASE_ANON_KEY || '',
-  });
-}
